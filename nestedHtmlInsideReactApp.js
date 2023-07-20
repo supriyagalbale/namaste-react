3f36212b8c8008405fdcea587e0d7de3c2c1bc12
Galbale <sgalbale@deloitte.com>
2023-07-20 10:20:14 +0530

    inception

---
+++ nestedHtmlInsideReactApp.js
@@ -0,0 +1,19 @@
+{
+  /* <div id="parent">
+    <div id="child">
+        <h1>This is nested HTML</h1>
+        <h2>second child</h2>
+    </div>
+</div> */
+}
+
+const parent = React.createElement(
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is nested HTML"),
+    React.createElement("h2", {}, "second child"),
+  ])
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
